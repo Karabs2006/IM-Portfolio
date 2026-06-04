2275f77f753b6217f323e1f0f9251d64ef8226c9
Kayy <157222013+Karabs2006@users.noreply.github.com>
2026-06-04 18:39:12 +0200

    Mobile + Tablet screen size fixes for Pages

---
+++ js/HeaderFooter.js
@@ -0,0 +1,96 @@
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+
+header.innerHTML=
+
+`
+<div class="off-screen-menu">
+
+        <div class="close-menu-btn">
+           <img src="../images/close.png" alt="Button to close burger menu">
+        </div>
+
+        <ul>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Skills</li>
+        <li>Hobbies</li>
+        <li>Music</li>
+        </ul>
+
+
+    </div>
+
+    <nav >
+        <div class="ham-menu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+       
+    </nav>
+
+    <nav class="static-nav">
+        <img src="../images/logo.jpg.png" alt="logo image" class="logo-img">
+        <ul class="nav-list">
+            <li><a href="./index.html" class="nav-link">Home</a></li>
+            <li><a href="./projects.html" class="nav-link">Projects</a></li>
+            <li><a href="./skills.html" class="nav-link">Skills</a></li>
+            <li><a href="./hobbies.html" class="nav-link">Hobbies</a></li>
+            <li><a href="./music.html" class="nav-link">Music</a></li>
+        </ul>
+    </nav>
+    `
+
+    footer.innerHTML =
+    `
+    <section class="contact-details">
+        <h1>Contact</h1>
+        <h2>Cell No.</h2>
+        <p>0744695264</p>
+        <br>
+        <h2>Email Address</h2>
+        <p>karabompati24@gmail.com</p>
+    </section>
+    <section class="social-links">
+        <h1>Social Links</h1>
+        <ul>
+            <li><a href="https://www.instagram.com/" target="_blank"><img src="../images/instagram.png">Instagram</a></li>
+            <li><a href="https://www.instagram.com/" target="_blank"><img src="../images/github.png">GitHub</a></li>
+            <li><a href="https://www.instagram.com/" target="_blank"><img src="../images/codecademy.png">Codecademy</a></li>
+            <li class="xbox-tag"><img src="../images/xbox.png">KayyDucksworth</li>
+
+        </ul>
+        
+    </section>
+    <button type="button" class="back-button" data-location="#index-hero-section">Back to top</button>
+    `
+
+    document.querySelector(".back-button").addEventListener("click", (event)=>{
+
+    window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+});
+
+
+
+
+})
+
+
+const menuButton = document.querySelector(".ham-menu");
+const hamMenu = document.querySelector(".off-screen-menu");
+const closeMenu = document.querySelector(".close-menu-btn");
+
+menuButton.addEventListener("click", (event)=>{
+
+    hamMenu.classList.toggle("active")
+     console.log("clicked");
+})
+
+
+closeMenu.addEventListener("click", (event)=>{
+
+    hamMenu.classList.toggle("active")
+})
--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,23 @@
+
 const pages = [
-    {path: "skills.gif", name: "Skills", paragrapgh: "Check out some of my skills. I have quite the nack for front-end and video game development"},
-    {path: "hobbies.gif", name: "Hobbies", paragrapgh: "Take a look at some of the things I enjoy doing when I’m not locked into work or studying. You might even pick up some of these hobbies yourself"},
-    {path: "music.gif", name: "Music", paragrapgh: "From synthy electronic scores to hardcore rock anthems, all types of music are welcome here. Check out some my favourite artists, genres and songs that are permanently stuck in my rotation."}
+
+    {   path: "skills.gif", 
+        name: "Skills", 
+        paragraph: "Check out some of my skills. I have quite the nack for front-end and video game development",
+        location: "skills.html"
+    },
+
+    {   path: "hobbies.gif", 
+        name: "Hobbies", 
+        paragraph: "Take a look at some of the things I enjoy doing when I’m not locked into work or studying. You might even pick up some of these hobbies yourself",
+        location: "hobbies.html"
+    },
+
+    {   path: "music.gif", 
+        name: "Music", 
+        paragraph: "From synthy electronic scores to hardcore rock anthems, all types of music are welcome here. Check out some my favourite artists, genres and songs that are permanently stuck in my rotation.",
+        location: "music.html"
+    }
 ]
 
 const container = document.querySelector(".about-container");
@@ -11,9 +27,27 @@ const renderCards = pages.forEach(page =>{
    container.innerHTML += 
    `<div class="about-card">
     <img src="../images/${page.path}" class="about-image">
-    <button type="button" class="about-button">${page.name}</button>
-    <p class="about-paragraph">${page.paragrapgh}</p>
+    <button type="button" class="about-button" data-location="${page.location}">${page.name}</button>
+    <p class="about-paragraph">${page.paragraph}</p>
+
     `
+})
+
+
+document.querySelectorAll(".about-button").forEach(button => {
+
+    button.addEventListener("click", () => {
+        window.open(`./${button.dataset.location}`, "_blank");
+    });
+});
+
+
+document.querySelector(".back-button").addEventListener("click", (event)=>{
+
+    window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+});
 })
 
 
